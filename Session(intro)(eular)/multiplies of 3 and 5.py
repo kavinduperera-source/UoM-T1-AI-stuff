243e2301519b7ce64bf52c76a35ecf2a683ce5e8
kavinduperera-source <kavinduperera749@gmail.com>
2025-09-29 13:51:12 +0530

    new file

---
+++ Session(intro)(eular)/multiplies of 3 and 5.py
@@ -0,0 +1,10 @@
+def sum_multiplies(limit):
+    the_sum = 0
+    for num in range(limit):
+        if num % 3 == 0 or num  % 5 == 0:
+            the_sum = the_sum + num
+    return the_sum
+
+limit = int(input("Enter the limit :"))
+print(sum_multiplies(limit))
+        
