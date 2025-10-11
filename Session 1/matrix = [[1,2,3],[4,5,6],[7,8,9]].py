1ca12e09ca724490a160c8632de06e609ca3af72
Kavindu_Perera <kavinduperera749@gmail.com>
2025-10-11 17:44:19 +0530

    cleared

--- Session 1/matrix = [[1,2,3],[4,5,6],[7,8,9]].py
+++
@@ -1,6 +0,0 @@
-matrix = [[1,2,3],[4,5,6],[7,8,9]]
-end_index = len(matrix) - 1
-diagonal = [matrix[i][i] for i in range(len(matrix))]
-next_diagonal =[matrix[i][end_index -i]for i in range(len(matrix))]
-print(diagonal)
-print(next_diagonal)
